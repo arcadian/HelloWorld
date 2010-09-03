93d966a1a6e819e8411b76714483dc5b906bf57b
efellios <mfelli@gmail.com>
2010-09-03 17:35:31 -0400

    QuickFix, testing egit push

--- src/com/git/core/HelloWorld.java
+++ src/com/git/core/HelloWorld.java
@@ -3,6 +3,6 @@ package com.git.core;
 public class HelloWorld {
 
 	public static void main(String args[]){
-		System.out.println("Hello Git World!");
+		System.out.println("Hello Git World stuffies!");
 	}
 }
